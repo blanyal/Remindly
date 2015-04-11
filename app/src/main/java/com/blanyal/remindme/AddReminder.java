4bb58ef6a6782b30f8d74c3e002dcc31ed1faec4
blanyal <blanyal@hotmail.com>
2015-04-11 21:30:04 +0530

    Add Reminder Activity

--- app/src/main/java/com/blanyal/remindme/AddReminder.java
+++ app/src/main/java/com/blanyal/remindme/AddReminder.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2014 Blanyal D'souza.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+
 package com.blanyal.remindme;
 
 
@@ -19,7 +38,7 @@ public class AddReminder extends ActionBarActivity {
 
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        mToolbar.setTitle(R.string.title_activity_add_reminder);
 
     }
 
