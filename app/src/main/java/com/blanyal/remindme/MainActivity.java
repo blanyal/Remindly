fee55ce7b0767a63c392562b045cbbe65c548f34
blanyal <blanyal@hotmail.com>
2015-04-11 19:13:05 +0530

    ui improvements

--- app/src/main/java/com/blanyal/remindme/MainActivity.java
+++ app/src/main/java/com/blanyal/remindme/MainActivity.java
@@ -2,16 +2,24 @@ package com.blanyal.remindme;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+
     }
 
     @Override
