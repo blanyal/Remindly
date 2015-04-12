da21e84b5cf2b3815e66b53f3765717badfede46
blanyal <blanyal@hotmail.com>
2015-04-12 14:30:47 +0530

    Updated licences

--- app/src/main/java/com/blanyal/remindme/AddReminder.java
+++ app/src/main/java/com/blanyal/remindme/AddReminder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Blanyal D'souza.
+ * Copyright 2015 Blanyal D'souza.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
